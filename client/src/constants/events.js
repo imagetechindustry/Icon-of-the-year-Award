11b5853194673b492815397606d86f26646622b3
Aditya9103 <adityakumarsingh84260@gmail.com>
2026-09-24 12:38:51 +0530

    first commit

---
+++ client/src/constants/events.js
@@ -0,0 +1,11 @@
+export const events = [
+    {
+        title: "Indian Icon of the Year Award, 2026 –Mumbai Edition",
+        date: "22 November,2026",
+        desc: "Honouring the visionary leaders and  industries rewriting the future of Indian business and guiding the next generation of entrepreneurs.",
+        place: "Mumbai, India",
+        highlight: "National Excellence",
+        banner: "../indiabrand-icon-award.jpeg",
+        slug: "https://www.indiabrandicon.in/upcoming-editions/2026/india-brand-icon-awards-conference"
+    },
+];
---
+++ client/src/constants/keyDates.js
@@ -0,0 +1,7 @@
+export const keyDates = [
+  {
+    title: "Mumbai Edition",
+    date: "22 November 2026",
+    icon: "🇮🇳",
+  },
+];
